c410d58177f2f3a9da6e6adc608a37e5da17e756
Favour Eduviere <fduvie@gmail.com>
2021-07-03 13:31:48 +0100

    Modified range_revised to print the array rangeOfNumbers

---
+++ range_revised.php
@@ -0,0 +1,27 @@
+<?php
+/*  SideHustle Task 1 (Revised):
+    Write a Range function that takes two(2) arguments, start and end, and returns
+    an array containing all the numbers from start up to (and including) end.
+*/
+
+    $start = 1;
+    $end = 13;
+    $step = 3;
+
+    $rangeOfNumbers = range($start, $end, $step);
+    echo 'The range of number in the array is : '. $rangeOfNumbers;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
