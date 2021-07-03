98ad599b5a78653145ac3b6c3c039364aa1823f6
Favour Eduviere <fduvie@gmail.com>
2021-07-03 14:17:28 +0100

    Modified Range revised
    Included a for loop to sum up the array

--- range_revised.php
+++ range_revised.php
@@ -11,18 +11,20 @@
     $rangeOfNumbers = range($start, $end, $step);
     echo 'The range of number in the array is : ';
     print_r($rangeOfNumbers);
+    echo "<br />The number of items in the array is: ";
+    print_r(count($rangeOfNumbers));
+    echo "<br />";
+
+    /* Write a sum function that takes an array of numbers 
+        and returns the sum of these numbers    */
+
+    $sumOfArray = 0;
+    for($n = 0; $n < count($rangeOfNumbers); $n++){
+            
+            $sumOfArray += $rangeOfNumbers[$n];
+            
+        }
+    echo "The Sum Of Items In The Array Is: $sumOfArray <br />";
+
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
